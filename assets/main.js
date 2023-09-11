33b186410b48185bfdfc58bedcde2fe39b6dc948
Riccardo <riky.volto@live.com>
2023-09-11 14:31:40 +0200

    print message in h1

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,19 @@
+/* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percoso in una propietá dell'oggetto restituito dalla funzione data. */
+
+
+
+
+
+
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        messagioTitolo : 'Ciao Vue js!',
       }
     }
   }).mount('#app')
